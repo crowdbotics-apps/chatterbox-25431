69348d2cbe3e8c8d0c1efc90005a66eb2fb4d88a
Crowdbotics <team@crowdbotics.com>
2021-04-02 18:48:55 +0000

    Update models

--- backend/chatterbox_25431/wsgi.py
+++ backend/chatterbox_25431/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatterbox_25431.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatterbox_25431.settings")
 
 application = get_wsgi_application()
